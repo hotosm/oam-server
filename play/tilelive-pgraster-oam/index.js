0bd4488a562c478135bc299dae76d8853b778900
Seth Fitzsimmons <seth@mojodna.net>
2015-07-23 17:49:02 -0700

    PostGIS-backed tilelive module

---
+++ play/tilelive-pgraster-oam/index.js
@@ -0,0 +1,127 @@
+"use strict";
+
+var url = require("url"),
+    util = require("util");
+
+var debug = require("debug")("tilelive-pgraster-oam"),
+    escape = require("pg-escape"),
+    mapnik = require("mapnik"),
+    merc = new (require("sphericalmercator"))(),
+    pg = require("pg");
+
+module.exports = function(tilelive) {
+  var PGRaster = function(uri, callback) {
+    this.databaseUrl = url.format(uri);
+
+    uri = url.parse(uri, true);
+
+    this.band = (uri.query.band | 0) || 1;
+    this.column = uri.query.column || "rast";
+    this.table = uri.query.table;
+    this.image = uri.query.image;
+
+    return setImmediate(callback, null, this);
+  };
+
+  PGRaster.prototype.getTile = function(z, x, y, callback) {
+    var column = this.column,
+        table = this.table,
+        image = this.image;
+
+    return pg.connect(this.databaseUrl, function(err, client, done) {
+      if (err) {
+        return callback(err);
+      }
+
+      var bbox = merc.bbox(x, y, z, false, "900913");
+
+      var query = escape(
+        [
+          "SELECT DISTINCT ON (image) image,",
+          "  zoom,",
+          "  ST_Width(ST_Clip(%I, ST_SetSRID($1::box2d, 3857))) width,",
+          "  ST_Height(ST_Clip(%I, ST_SetSRID($1::box2d, 3857))) height,",
+          "  ST_AsPNG(ST_Clip(%I, ST_SetSRID($1::box2d, 3857))) png",
+          "FROM %I",
+          "WHERE image = $2",
+          "  AND zoom <= $3",
+          "  AND %I && $1::box2d",
+          "  ORDER BY image, zoom DESC"
+        ].join("\n"),
+        column,
+        column,
+        column,
+        table,
+        column
+      );
+
+      return client.query(query,
+                          [
+                            util.format("BOX(%d %d,%d %d)", bbox[0], bbox[1], bbox[2], bbox[3]),
+                            image,
+                            z
+                          ],
+                          function(err, result) {
+        done();
+
+        if (err) {
+          return callback(err);
+        }
+
+        if (result.rows.length === 0) {
+          return callback(new Error("Tile does not exist"));
+        }
+
+        if (result.rows.length > 1) {
+          return callback(new Error("Too many rows returned."));
+        }
+
+        debug("zoom:", result.rows[0].zoom);
+        debug("width:", result.rows[0].width);
+        debug("height:", result.rows[0].height);
+
+        return mapnik.Image.fromBytes(result.rows[0].png, function(err, im) {
+          if (err) {
+            return callback(err);
+          }
+
+          // we're getting a 257x257 image back, so let's window it
+          var view = im.view(1, 0, 257, 256);
+
+          return view.encode(function(err, png) {
+            if (err) {
+              return callback(err);
+            }
+
+            return callback(null, png, {
+              "Content-Type": "image/png"
+            });
+          });
+        });
+      });
+    });
+  };
+
+  PGRaster.prototype.getInfo = function(callback) {
+    // LC81410412014277LGN00
+    // 85.273277798802 27.4280691147198
+    // TODO query for zooms and center
+    return setImmediate(callback, null, {
+      format: "png",
+      minzoom: 0,
+      maxzoom: Infinity
+    });
+  };
+
+  PGRaster.prototype.close = function(callback) {
+    return callback && setImmediate(callback);
+  };
+
+  PGRaster.registerProtocols = function(tilelive) {
+    tilelive.protocols["pgraster+oam:"] = PGRaster;
+  };
+
+  PGRaster.registerProtocols(tilelive);
+
+  return PGRaster;
+};
